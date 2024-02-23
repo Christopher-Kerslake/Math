03baeac03fce0e57e57cbf088b0af785f36ac475
Christopher Kerslake <kerslake.r.chris@gmail.com>
2024-02-23 12:12:37 -0700

    Started MatrixConversion.py file.

---
+++ MatrixConversion.py
@@ -0,0 +1,4 @@
+#The purpose of this python library will be to compute linear equations into a matrix
+#It will take in n equations with n variables and will convert them to a matrix as long as they have the same elements
+#It can also turn linear equations into their standard form through algebraic operations
+#It will take in equations as strings, and output arrays/matrices
