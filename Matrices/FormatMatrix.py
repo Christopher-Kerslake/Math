2451afc0cec6b77bb6365c552d30f292ddef22ad
Christopher Kerslake <kerslake.r.chris@gmail.com>
2024-03-05 16:18:31 -0700

    Added in Matrix Formatting File.

---
+++ Matrices/FormatMatrix.py
@@ -0,0 +1,16 @@
+#The purpose of this file will be to take in multiple equations in string format
+#in order to convert them into matrix form. The functionality is as follows:
+#
+#Takes in multiple strings, for example "2x + 3y + 4z = 1", "3x + 2y + z 5"
+#It formats them into proper matrix form
+#It will pass back the following list of lists (matrix)
+#
+#[[2, 3, 4, 1], [3, 2, 1, 5]]
+#
+#This is general purpose in order to convert n equations into a matrix
+#of n size.
+#
+#This does not turn the equations into standard format, it simply formats the matrix
+#
+
+
